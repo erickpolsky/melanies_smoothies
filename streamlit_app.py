3b9841e325a0f4dc672279b24fd7815bf76a163f
erickpolsky <erick@polsky.org>
2026-05-13 12:30:41 -0400

    Switch to using Pandas for dataframe display
    Replaced Snowflake dataframe display with Pandas dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 import requests  
+import pandas as pd
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -16,8 +17,14 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#pandas dataframe
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe ,max_selections=6
